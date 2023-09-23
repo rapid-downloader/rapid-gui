d553d31b6afc21cbc9c9316d3187f83718361d72
thoriqadillah <thoriqadillah59@gmail.com>
2023-09-23 22:17:50 +0700

    add shadcn

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,9 @@
 <template>
-    Hello World
+    <div class="flex h-screen">
+        <Button class="m-auto rounded">Hello World</Button>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
