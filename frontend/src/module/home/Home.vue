776fc4b92e21680c6dd9564045c0c1b9496b80b5
thoriqadillah <thoriqadillah59@gmail.com>
2023-09-30 23:33:15 +0700

    add select

--- frontend/src/module/home/Home.vue
+++ frontend/src/module/home/Home.vue
@@ -2,6 +2,15 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
 </script>
 
 <template>
@@ -10,7 +19,19 @@ import { Input } from '@/components/ui/input'
             <Button class="flex gap-2 py-4">
                 <i-fluent-add-16-filled class="text-primary-foreground" />
             </Button>
-            
+            <Select>
+                <SelectTrigger>
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple">
+                            Apple
+                        </SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
         </div>
 
         <Input placeholder="Search" class="w-[20rem] rounded"></Input>
