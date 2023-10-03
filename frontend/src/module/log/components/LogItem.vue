0f76e7b8fe7a6d3f0ee56c325aa46a64aa3161e2
thoriqadillah <thoriqadillah59@gmail.com>
2023-10-03 22:26:53 +0700

    fix log ui

--- frontend/src/module/log/components/LogItem.vue
+++ frontend/src/module/log/components/LogItem.vue
@@ -3,6 +3,7 @@ import Cato from '@/assets/images/cato.svg'
 import { computed, ref } from 'vue';
 import P from '@/components/ui/P.vue';
 import { useRouteQuery } from '@vueuse/router';
+import { Separator } from 'radix-vue';
 
 const search = useRouteQuery('search', '')
 
@@ -28,9 +29,9 @@ const items = computed(() => logs.value.filter(log => log.toLowerCase().includes
             <img :src="Cato" alt="empty" class="mx-auto my-auto w-[20rem] h-screen -mt-[5rem]">
         </div>
         <div v-else class="w-fit flex flex-col px-2">
-            <div v-for="(item, i) in items" :key="i" class="flex gap-2">
-                <span class="text-base font-medium font-mono mt-0.5">> </span>
-                <P class="text-left [&:not(:first-child)]:mt-0 text-sm xl:w-[75rem] font-mono">{{ item }}</P>
+            <div v-for="(item, i) in items" :key="i" class="flex gap-2 mb-4">
+                <span class="text-base font-medium font-mono">> </span>
+                <span class="text-left [&:not(:first-child)]:mt-0 text-sm xl:w-[75rem] font-mono">{{ item }}</span>
             </div>
         </div>
     </div>
