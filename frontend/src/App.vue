a7dbdba3a5c315314546433df88618473f342ef5
thoriqadillah <thoriqadillah59@gmail.com>
2023-10-10 23:16:40 +0700

    leftover

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { useStorage } from '@vueuse/core';
 import Navbar from './components/Navbar.vue';
 
-const jwt = useStorage<string>('jwt', 'kucing')
 </script>
 
 <template>
-    {{ jwt }}
     <div class="flex bg-background min-w-[50rem]">
         <Navbar />
         <router-view class="mx-5 my-1.5"/>
